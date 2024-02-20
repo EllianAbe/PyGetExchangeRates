f0ca9ab386e081e483da13fec6331f5c30e682cb
Ellian Abe <ellian.yabe@gmail.com>
2024-02-20 20:28:41 -0300

    break into browsers

---
+++ chrome.py
@@ -0,0 +1,30 @@
+from selenium.webdriver import ChromeOptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import os
+
+os.environ["TMPDIR"] = "temp"
+
+df = pd.read_csv('input.csv')
+
+driver = webdriver.Chrome()
+
+try:
+
+    for index, row in df.iterrows():
+        driver.get(f"https://www.google.com/search?q={row['command']}")
+        element = driver.find_elements(
+            by=By.XPATH, value='//input[@aria-label="Campo do montante da moeda"]')[1]
+
+        text = element.get_attribute('value')
+        df.at[index, 'rate'] = float(text)
+
+        driver.implicitly_wait(3)
+except Exception as e:
+    raise e
+finally:
+    driver.quit()
+
+
+df.to_csv('output.csv', index=False)
